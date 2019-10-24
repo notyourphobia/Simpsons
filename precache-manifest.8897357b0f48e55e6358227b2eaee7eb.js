4618dca4e6c9e569c5ddbdbcd63c0c08b77117eb
S0nicTheHedgehog <notyorphobia@gmail.com>
2019-10-24 15:12:29 +0300

    Updates

---
+++ precache-manifest.8897357b0f48e55e6358227b2eaee7eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22415e4353f7924025c0d694868b6a0",
+    "url": "/Simpsons/index.html"
+  },
+  {
+    "revision": "dd6455b8eaf26dbd85fc",
+    "url": "/Simpsons/static/css/main.2a2c1efd.chunk.css"
+  },
+  {
+    "revision": "d9428302818ec00cb19f",
+    "url": "/Simpsons/static/js/2.40e8bc63.chunk.js"
+  },
+  {
+    "revision": "dd6455b8eaf26dbd85fc",
+    "url": "/Simpsons/static/js/main.93a901c5.chunk.js"
+  },
+  {
+    "revision": "f58ff37060c72d0e7c41",
+    "url": "/Simpsons/static/js/runtime-main.f8643b40.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Simpsons/static/media/logo.25bf045c.svg"
+  }
+]);
